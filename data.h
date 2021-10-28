3b9e5f16ed29e97e5a2b509a2a567dc14113b8fc
logan <loganf0101@gmail.com>
2021-10-28 18:23:06 -0500

    Created data.h. Holds a struct called Data which holds an id int and a string information

---
+++ data.h
@@ -0,0 +1,16 @@
+//
+// Created by logan on 10/28/2021.
+//
+
+#ifndef QUEUE_DATA_H
+#define QUEUE_DATA_H
+
+#include <string>
+using std::string;
+
+struct Data {
+    int id;
+    string information;
+};
+
+#endif //QUEUE_DATA_H
