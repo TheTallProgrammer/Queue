5a227fc3d12ae4161e17715cc1ccf7dc1e040dc6
Logan Falkenberg <loganf0101@gmail.com>
2021-10-28 15:29:12 -0500

    Created queue .cpp/.h

---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by Logan on 10/28/2021.
+//
+
+#ifndef QUEUE_PROJECT_MAIN_H
+#define QUEUE_PROJECT_MAIN_H
+
+#endif //QUEUE_PROJECT_MAIN_H
---
+++ queue.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Logan on 10/28/2021.
+//
+
+#include "queue.h"
